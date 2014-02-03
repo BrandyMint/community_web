11a8aa29842b2bb8c3df5475c04a3523b2b9e636
heretge <vozdelaluz@gmail.com>
2014-02-03 11:00:46 +0400

    activeadmin actions

--- app/admin/place.rb
+++ app/admin/place.rb
@@ -6,12 +6,13 @@ ActiveAdmin.register Place do
       f.input :position
       f.input :discounts
     end
+    f.actions
   end
 
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :name, :position, discount_ids: []
   #
   # or
   #
